bda016d4b096a29bc00917397d20b5602de6fd47
RDB-Biller <ripodatabase@gmail.com>
2026-04-13 09:49:06 +0000

    index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
+import express from "express";
+import paymentRoutes from "./routes/payments";
 
+const app = express();
+app.use(express.json());
+
+app.use("/payments", paymentRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Fintech Demo Running");
+});
+
+app.listen(process.env.PORT || 3000);
